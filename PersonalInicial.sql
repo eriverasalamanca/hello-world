4f7b03ebbad96903fa0a314a6d7439da90285cdf
eriverasalamanca <eriverasalamanca@gmail.com>
2017-05-25 13:00:49 -0500

    PersonalInicial.sql
    Esta es una vista de SQL Server, a continuación agregaré el query que representa la vista.

---
+++ PersonalInicial.sql
@@ -0,0 +1,8 @@
+PersonalInicial:
+SQL SELECT *
+FROM ADAM.dbo.vwPersonalInicial;
+store PersonalInicial into ..\qvd\PersonalInicial.qvd;
+drop Table PersonalInicial;
+
+SELECT *
+FROM ADAM.dbo.vwPersonalInicial;
