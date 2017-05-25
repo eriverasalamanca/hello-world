c896ec62ba39a0f252a7d88817f5959990f95373
eriverasalamanca <eriverasalamanca@gmail.com>
2017-05-25 13:19:00 -0500

    Delete PersonalInicial.sql

--- PersonalInicial.sql
+++
@@ -1,8 +0,0 @@
-PersonalInicial:
-SQL SELECT *
-FROM ADAM.dbo.vwPersonalInicial;
-store PersonalInicial into ..\qvd\PersonalInicial.qvd;
-drop Table PersonalInicial;
-
-SELECT *
-FROM ADAM.dbo.vwPersonalInicial;
